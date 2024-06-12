381c0b71bbd0b77bada456d4ff7b74ee14be7a2c
Matheus Melo <klamatheus@hotmail.com>
2024-06-11 21:49:12 -0300

    Javados + Novo ReadMe + Diagrama de Classe

---
+++ src/main/java/com/example/springdatapoo/SpringDataPooSeedelApplication.java
@@ -0,0 +1,27 @@
+package com.example.springdatapoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe principal da aplicação
+ * Esta classe contém o método main, que inicia a execução da aplicação Spring Boot.
+ *
+ * @author Lucca Bellini Pena,
+ *         Danielle Rachel Rios Moreira,
+ *         Matheus Henrique Melo Lemes da Silva,
+ *         Nicolas Luis Barbosa Bulhões
+ *         Alexandre Cavalcante da Silva
+ */
+@SpringBootApplication
+public class SpringDataPooSeedelApplication {
+
+    /**
+     * Método main para iniciar a aplicação Spring Boot.
+     *
+     * @param args Os argumentos de linha de comando passados para a aplicação.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataPooSeedelApplication.class, args);
+    }
+}
